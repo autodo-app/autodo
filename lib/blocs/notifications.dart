8d8447d7bbb68f4ab9481a1652301d8e291e3d17
Jonathan Bayless <baylessj@purdue.edu>
2019-10-12 13:49:56 -0400

    basic test notification works

--- lib/blocs/notifications.dart
+++ lib/blocs/notifications.dart
@@ -1,35 +1,97 @@
 import 'dart:async';
-
 import 'package:flutter/foundation.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter/widgets.dart';
-import 'package:autodo/screens/loadingpage.dart';
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class NotificationBLoC {
-  // TODO: add functions formatting the Map for each of the kotlin code's possible options
-  static final MethodChannel android =
-      MethodChannel('com.jonathanbayless.autodo/android');
+  FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin;
 
-  Future<void> launchReturn(String payload) async {
-    // Navigator.pushNamed(key.currentContext, '/load');
-  }
-
-  Future<void> _handler(MethodCall call) {
-    switch (call.method) {
-      case 'launch':
-        return launchReturn(call.arguments);
-      default:
-        return Future.error('method not defined');
+  Future onSelectNotification(String payload) async {
+    if (payload != null) {
+      debugPrint('notification payload: ' + payload);
     }
+    // await Navigator.push(
+    //   context,
+    //   MaterialPageRoute(builder: (context) => SecondScreen(payload)),
+    // );
   }
 
-  NotificationBLoC() {
-    // android.setMethodCallHandler(_handler);
+  Future<void> pushBasicNotification({title, content}) async {
+    var androidPlatformChannelSpecifics = AndroidNotificationDetails(
+        'your channel id', 'your channel name', 'your channel description',
+        importance: Importance.Max, priority: Priority.High, ticker: 'ticker');
+    var iOSPlatformChannelSpecifics = IOSNotificationDetails();
+    var platformChannelSpecifics = NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.show(
+        0, 'plain title', 'plain body', platformChannelSpecifics,
+        payload: 'item x');
   }
 
-  static Future<void> pushBasicNotification(
-      {@required title, @required content}) async {
-    var args = {"title": title, "content": content};
-    android.invokeMethod('pushNotification', args);
+  Future<void> scheduleNotification() async {
+    var scheduledNotificationDateTime =
+        new DateTime.now().add(new Duration(seconds: 5));
+    var androidPlatformChannelSpecifics =
+        new AndroidNotificationDetails('your other channel id',
+            'your other channel name', 'your other channel description');
+    var iOSPlatformChannelSpecifics =
+        new IOSNotificationDetails();
+    NotificationDetails platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.schedule(
+        0,
+        'scheduled title',
+        'scheduled body',
+        scheduledNotificationDateTime,
+        platformChannelSpecifics);
+  }
+
+  Future<void> repeatedlyNotify() async {
+    // Show a notification every minute with the first appearance happening a minute after invoking the method
+    var androidPlatformChannelSpecifics =
+        new AndroidNotificationDetails('repeating channel id',
+            'repeating channel name', 'repeating description');
+    var iOSPlatformChannelSpecifics =
+        new IOSNotificationDetails();
+    var platformChannelSpecifics = new NotificationDetails(
+        androidPlatformChannelSpecifics, iOSPlatformChannelSpecifics);
+    await flutterLocalNotificationsPlugin.periodicallyShow(0, 'repeating title',
+        'repeating body', RepeatInterval.EveryMinute, platformChannelSpecifics);
+  }
+
+  Future<void> cancel({id}) async {
+    // cancel the notification with id value of zero
+    await flutterLocalNotificationsPlugin.cancel(0);
+  }
+  
+  Future<void> cancelAll() async {
+    await flutterLocalNotificationsPlugin.cancelAll();
+  }
+
+  Future<bool> launchedByNotification() async {
+    var notificationAppLaunchDetails =
+      await flutterLocalNotificationsPlugin.getNotificationAppLaunchDetails();
+    return notificationAppLaunchDetails.didNotificationLaunchApp;
+  }
+
+  Future<dynamic> onDidReceiveLocalNotification(int ign1, String ign2, String ign3, String ign4) async {}
+  
+  // Make the object a Singleton
+  static final NotificationBLoC _bloc = NotificationBLoC._internal();
+  factory NotificationBLoC() {
+    return _bloc;
+  }
+  NotificationBLoC._internal() {
+    // Initialize the plugin's settings
+    flutterLocalNotificationsPlugin = FlutterLocalNotificationsPlugin();
+    // initialise the plugin. app_icon needs to be a added as a drawable resource to the Android head project
+    var initializationSettingsAndroid =
+        AndroidInitializationSettings('@drawable/app_icon');
+    var initializationSettingsIOS = IOSInitializationSettings(
+        onDidReceiveLocalNotification: onDidReceiveLocalNotification);
+    var initializationSettings = InitializationSettings(
+        initializationSettingsAndroid, initializationSettingsIOS);
+    flutterLocalNotificationsPlugin.initialize(initializationSettings,
+        onSelectNotification: onSelectNotification);
   }
 }
