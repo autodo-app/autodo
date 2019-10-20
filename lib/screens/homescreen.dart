3af174cd720a43f4178f55cca5f6bbbdb99a3a9c
Jonathan Bayless <baylessj@purdue.edu>
2019-10-20 15:20:31 -0400

    RE: #53 - move to bottom tab controlller

--- lib/screens/homescreen.dart
+++ lib/screens/homescreen.dart
@@ -1,3 +1,6 @@
+import 'package:autodo/refueling/history.dart';
+import 'package:autodo/screens/editrepeats.dart';
+import 'package:autodo/screens/screens.dart';
 import 'package:flutter/material.dart';
 import 'package:autodo/sharedmodels/sharedmodels.dart';
 import 'package:autodo/maintenance/history.dart';
@@ -11,6 +14,15 @@ class HomeScreen extends StatefulWidget {
 enum dropdown { settings }
 
 class HomeScreenState extends State<HomeScreen> {
+  int tabIndex = 0;
+
+  List<Widget> bodies = [
+    MaintenanceHistory(),
+    RefuelingHistory(),
+    StatisticsScreen(),
+    EditRepeatsScreen(),
+  ];
+  
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -40,7 +52,33 @@ class HomeScreenState extends State<HomeScreen> {
             ),
           ],
         ),
-        body: MaintenanceHistory(),
+        // body: MaintenanceHistory(),
+        body: bodies[tabIndex],
+        bottomNavigationBar: BottomNavigationBar(  
+          type: BottomNavigationBarType.fixed,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          onTap: (index) => setState(() => tabIndex = index),
+          currentIndex: tabIndex,
+          items: [
+            BottomNavigationBarItem(
+              icon: Icon(Icons.build),
+              title: Text('ToDos')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.local_gas_station),
+              title: Text('Refuelings')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.show_chart),
+              title: Text('Statistics')
+            ),
+            BottomNavigationBarItem(
+              icon: Icon(Icons.repeat),
+              title: Text('Repeats')
+            ),
+          ]
+        ),
         drawer: NavDrawer(),
         floatingActionButton: CreateEntryButton(),
         backgroundColor: Colors.transparent,
