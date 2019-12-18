9892ad4b08bcab803bfcbb6991a6f0c9bf63a9e3
Jonathan Bayless <baylessj@purdue.edu>
2019-12-18 11:15:54 -0500

    Add actual test coverage

--- test/dummy_test.dart
+++ test/dummy_test.dart
@@ -1,8 +1,10 @@
 // Import the test package and Counter class
 import 'package:test/test.dart';
+import 'package:autodo/theme.dart';
 
 void main() {
   test('Example', () {
+    createTheme();
     expect(1, 1);
   });
 }
