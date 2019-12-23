2652aaf22fbe9c36ff842e997e2cca2755b29725
Jonathan Bayless <baylessj@purdue.edu>
2019-12-23 14:21:02 -0500

    default sentry code

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,11 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:sentry/sentry.dart';
 
 import 'widgets/widgets.dart';
 import 'blocs/blocs.dart';
@@ -11,6 +15,24 @@ import 'repositories/repositories.dart';
 import 'delegate.dart';
 import 'routes.dart';
 import 'theme.dart';
+
+final SentryClient _sentry = SentryClient(dsn: "https://20c77d8ea8634cb68f64d31a70a43add@sentry.io/1865109");
+
+Future<void> _reportError(dynamic error, dynamic stackTrace) async {
+  // Print the exception to the console.
+  print('Caught error: $error');
+  if (!kReleaseMode) {
+    // Print the full stacktrace in debug mode.
+    print(stackTrace);
+    return;
+  } else {
+    // Send the Exception and Stacktrace to Sentry in Production mode.
+    _sentry.captureException(
+      exception: error,
+      stackTrace: stackTrace,
+    );
+  }
+}
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -26,19 +48,21 @@ void main() {
   final AuthRepository authRepository = FirebaseAuthRepository();
   // ThemeData theme = AutodoTheme();
   ThemeData theme = createTheme();
-  runApp(
-    BlocProvider<AuthenticationBloc>(
-      create: (context) => AuthenticationBloc(
-          userRepository: authRepository
-        )..add(AppStarted()),
-      child: BlocProvider<DatabaseBloc>(  
-        create: (context) => DatabaseBloc(
-          authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
-        )..add(LoadDatabase()),
-        child: App(theme: theme),
+  runZoned<Future<void>>(() async {
+    runApp(
+      BlocProvider<AuthenticationBloc>(
+        create: (context) => AuthenticationBloc(
+            userRepository: authRepository
+          )..add(AppStarted()),
+        child: BlocProvider<DatabaseBloc>(  
+          create: (context) => DatabaseBloc(
+            authenticationBloc: BlocProvider.of<AuthenticationBloc>(context),
+          )..add(LoadDatabase()),
+          child: App(theme: theme),
+        ),
       ),
-    ),
-  );
+    );
+  }, onError: (error, stackTrace) => _reportError(error, stackTrace));
 }
 
 class App extends StatelessWidget {
