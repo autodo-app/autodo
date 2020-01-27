8ceee96b482869751995dcdc97cff47f26d22782
Jonathan Bayless <baylessj@purdue.edu>
2020-01-27 17:24:35 -0500

    able to access home screen on ios!

---
+++ ios/Pods/Target Support Files/flutter_keyboard_visibility/flutter_keyboard_visibility-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "KeyboardVisibilityPlugin.h"
+
+FOUNDATION_EXPORT double flutter_keyboard_visibilityVersionNumber;
+FOUNDATION_EXPORT const unsigned char flutter_keyboard_visibilityVersionString[];
+
