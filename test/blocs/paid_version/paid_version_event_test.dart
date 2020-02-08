43e90e6e7a465a4aaef96b8c2eb9acaf61bcfb87
Jonathan Bayless <baylessj@purdue.edu>
2020-02-08 16:18:23 -0500

    add tests for paid version bloc

---
+++ test/blocs/paid_version/paid_version_event_test.dart
@@ -0,0 +1,9 @@
+import 'package:flutter_test/flutter_test.dart';
+
+import 'package:autodo/blocs/blocs.dart';
+
+void main() {
+  test('event props', () {
+    expect(PaidVersionUpgrade().props, []);
+  });
+}
