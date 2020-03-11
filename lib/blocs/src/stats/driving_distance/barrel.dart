1c918623c6763da03f10b682a3dad9cb0e8ad55f
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-03-11 18:32:17 -0400

    Update Driving Distance Stats Unit

--- lib/blocs/src/stats/driving_distance/barrel.dart
+++ lib/blocs/src/stats/driving_distance/barrel.dart
@@ -1,3 +1,71 @@
 export 'bloc.dart';
 export 'event.dart';
 export 'state.dart';
+
+import 'package:autodo/models/models.dart';
+import 'package:autodo/models/src/distanceratepoint.dart';
+import 'package:autodo/units/units.dart';
+import 'package:charts_flutter/flutter.dart';
+import 'package:flutter/widgets.dart';
+
+import '../../../blocs.dart';
+
+class DrivingDistanceStats {
+  static Future<List<Series<DistanceRatePoint, DateTime>>> fetch(
+    CarsBloc carsBloc,
+    BuildContext context,
+  ) async {
+    var state = carsBloc.state;
+    if (state is CarsLoaded) {
+      return _prepData(state.cars, context);
+    }
+
+    await for (final _state in carsBloc) {
+      if (_state is CarsLoaded) {
+        state = _state;
+        break;
+      }
+    }
+
+    if (state is CarsLoaded) {
+      return _prepData(state.cars, context);
+    }
+
+    throw 'Unable to load car';
+  }
+
+  static Future<List<Series<DistanceRatePoint, DateTime>>> _prepData(
+    List<Car> cars,
+    BuildContext context,
+  ) async {
+    final out = <Series<DistanceRatePoint, DateTime>>[];
+    final distance = Distance.of(context);
+    final unit = distance.unitString(context, short: true);
+
+    for (var car in cars) {
+      final points = car.distanceRateHistory;
+
+      if (points == null || points.isEmpty) continue;
+
+      out.add(
+        Series<DistanceRatePoint, DateTime>(
+          id: car.name,
+          // colorFn: (_, __) {
+          //   var primaryColor = Theme.of(context).primaryColor;
+          //   return Color(r: primaryColor.red, g: primaryColor.green, b: primaryColor.blue);
+          // },
+          domainFn: (DistanceRatePoint point, _) => point.date,
+          measureFn: (DistanceRatePoint point, _) =>
+              distance.internalToUnit(point.distanceRate),
+          measureFormatterFn: (DistanceRatePoint point, _) =>
+              (num v) => '${distance.format(v)} $unit',
+          domainFormatterFn: (DistanceRatePoint t, int _) => (v) => 'd',
+          labelAccessorFn: (DistanceRatePoint point, _) => 'HELLO $point',
+          data: points,
+        ),
+      );
+    }
+
+    return out;
+  }
+}
--- lib/screens/home/views/charts/drivingdistance.dart
+++ lib/screens/home/views/charts/drivingdistance.dart
@@ -1,7 +1,11 @@
+import 'package:autodo/blocs/blocs.dart';
 import 'package:autodo/localization.dart';
 import 'package:autodo/models/models.dart';
+import 'package:autodo/units/units.dart';
+import 'package:autodo/widgets/widgets.dart';
 import 'package:charts_flutter/flutter.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:json_intl/json_intl.dart';
 
 import 'shared.dart';
@@ -13,22 +17,22 @@ class DrivingDistanceChart extends StatelessWidget {
 
   final bool animate;
 
-  int lowerBound() {
+  int lowerBound(Distance distance) {
     final minVal = seriesList[0]
         .data
         .reduce((value, element) =>
             (value.distanceRate < element.distanceRate) ? value : element)
         .distanceRate;
-    return (0.75 * minVal).round();
+    return (0.75 * distance.internalToUnit(minVal)).round();
   }
 
-  int upperBound() {
+  int upperBound(Distance distance) {
     final maxVal = seriesList[0]
         .data
         .reduce((value, element) =>
             (value.distanceRate > element.distanceRate) ? value : element)
         .distanceRate;
-    return (1.25 * maxVal).round();
+    return (1.25 * distance.internalToUnit(maxVal)).round();
   }
 
   @override
@@ -38,43 +42,85 @@ class DrivingDistanceChart extends StatelessWidget {
           child: Text(JsonIntl.of(context).get(IntlKeys.noData),
               style: Theme.of(context).primaryTextTheme.bodyText2));
     }
+
+    final distance = Distance.of(context);
+
     return TimeSeriesChart(
       seriesList,
       animate: animate,
-      primaryMeasureAxis: NumericAxisSpec.from(numberAxisSpec,
-          tickProviderSpec: StaticNumericTickProviderSpec(
-              lerp(lowerBound(), upperBound(), 6, 4))),
+      primaryMeasureAxis: NumericAxisSpec.from(
+        numberAxisSpec,
+        tickProviderSpec: StaticNumericTickProviderSpec(
+            lerp(lowerBound(distance), upperBound(distance), 6, 4)),
+      ),
       // add configurations here eventually
       domainAxis: dateAxisSpec,
+
       // primaryMeasureAxis: numberAxisSpec,
     );
   }
 }
 
-class DrivingDistanceHistory extends StatelessWidget {
-  const DrivingDistanceHistory(this.data);
-
-  final List<Series> data;
+class DrivingDistanceHistory extends StatefulWidget {
+  const DrivingDistanceHistory();
 
   @override
-  Widget build(BuildContext context) => Column(
-          mainAxisSize: MainAxisSize.min,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            Padding(
-              padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
-            ),
-            Text(JsonIntl.of(context).get(IntlKeys.drivingDistanceHistory),
-                style: Theme.of(context).primaryTextTheme.subtitle2),
-            Container(
-              height: 300,
-              padding: EdgeInsets.all(15),
-              child: DrivingDistanceChart(data),
-            ),
-            Text(
-              'Refueling Date',
-              style: Theme.of(context).primaryTextTheme.bodyText2,
-              textAlign: TextAlign.center,
-            ),
-          ]);
+  _DrivingDistanceHistoryState createState() => _DrivingDistanceHistoryState();
+}
+
+class _DrivingDistanceHistoryState extends State<DrivingDistanceHistory> {
+  List<Series> data;
+  String error;
+
+  @override
+  void didChangeDependencies() {
+    final carsBloc = BlocProvider.of<CarsBloc>(context);
+
+    DrivingDistanceStats.fetch(carsBloc, context).then((value) {
+      setState(() {
+        data = value;
+      });
+    }).catchError((e) {
+      setState(() {
+        error = e.toString();
+      });
+    });
+
+    super.didChangeDependencies();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (error != null) {
+      return Text(error);
+    }
+
+    if (data == null) {
+      return LoadingIndicator();
+    }
+
+    return Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Padding(
+            padding: EdgeInsets.fromLTRB(0, 15, 0, 0),
+          ),
+          Text(
+              JsonIntl.of(context).get(IntlKeys.drivingDistanceHistory, {
+                'unit': Distance.of(context).unitString(context),
+              }),
+              style: Theme.of(context).primaryTextTheme.subtitle2),
+          Container(
+            height: 300,
+            padding: EdgeInsets.all(15),
+            child: DrivingDistanceChart(data),
+          ),
+          Text(
+            'Refueling Date',
+            style: Theme.of(context).primaryTextTheme.bodyText2,
+            textAlign: TextAlign.center,
+          ),
+        ]);
+  }
 }
