477db5609fc74d4319cba8f64b3687d48a0df109
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-03-18 06:53:04 -0400

    Update screen stats

--- test/screens/home/views/stats_test.dart
+++ test/screens/home/views/stats_test.dart
@@ -1,45 +1,50 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:mockito/mockito.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+import 'dart:math';
+
+import 'package:autodo/blocs/blocs.dart';
+import 'package:autodo/models/models.dart';
+import 'package:autodo/screens/home/views/charts/barrel.dart';
+import 'package:autodo/screens/home/views/stats.dart';
+import 'package:autodo/units/units.dart';
+import 'package:autodo/widgets/widgets.dart';
 import 'package:bloc_test/bloc_test.dart';
 import 'package:charts_flutter/flutter.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mockito/mockito.dart';
+import 'package:preferences/preferences.dart';
+import 'package:provider/provider.dart';
 
-import 'package:autodo/models/models.dart';
-import 'package:autodo/blocs/blocs.dart';
-import 'package:autodo/widgets/widgets.dart';
-import 'package:autodo/screens/home/views/stats.dart';
-import 'package:autodo/screens/home/views/charts/barrel.dart';
+class MockRefuelingsBloc extends MockBloc<RefuelingsEvent, RefuelingsState>
+    implements RefuelingsBloc {}
 
-class MockEfficiencyStatsBloc
-    extends MockBloc<EfficiencyStatsEvent, EfficiencyStatsState>
-    implements EfficiencyStatsBloc {}
-
-class MockDrivingDistanceStatsBloc
-    extends MockBloc<DrivingDistanceStatsEvent, DrivingDistanceStatsState>
-    implements DrivingDistanceStatsBloc {}
+class MockCarsBloc extends MockBloc<CarsEvent, CarsState> implements CarsBloc {}
 
 void main() {
-  EfficiencyStatsBloc effBloc;
-  DrivingDistanceStatsBloc driveBloc;
+  BasePrefService pref;
 
-  setUp(() {
-    effBloc = MockEfficiencyStatsBloc();
-    driveBloc = MockDrivingDistanceStatsBloc();
+  setUp(() async {
+    pref = JustCachePrefService();
+    await pref.setDefaultValues({
+      'length_unit': DistanceUnit.imperial.index,
+      'volume_unit': VolumeUnit.us.index,
+      'currency': 'USD',
+    });
   });
 
   group('RefuelingsScreen', () {
     testWidgets('loading', (WidgetTester tester) async {
-      when(effBloc.state).thenAnswer((_) => EfficiencyStatsLoading());
-      when(driveBloc.state).thenAnswer((_) => DrivingDistanceStatsLoading());
+      final carsBloc = MockCarsBloc();
+      final refuelingsBloc = MockRefuelingsBloc();
+
       await tester.pumpWidget(
         MultiBlocProvider(
           providers: [
-            BlocProvider<EfficiencyStatsBloc>.value(
-              value: effBloc,
+            BlocProvider<CarsBloc>.value(
+              value: carsBloc,
             ),
-            BlocProvider<DrivingDistanceStatsBloc>.value(
-              value: driveBloc,
+            BlocProvider<RefuelingsBloc>.value(
+              value: refuelingsBloc,
             ),
           ],
           child: MaterialApp(
@@ -52,95 +57,92 @@ void main() {
       await tester.pump();
       expect(find.byType(LoadingIndicator), findsNWidgets(2));
     });
+
     testWidgets('no data', (WidgetTester tester) async {
-      when(effBloc.state).thenAnswer((_) => EfficiencyStatsLoaded([]));
-      when(driveBloc.state).thenAnswer((_) => DrivingDistanceStatsLoaded([]));
+      final carsBloc = MockCarsBloc();
+      final refuelingsBloc = MockRefuelingsBloc();
+
+      when(carsBloc.state).thenAnswer((_) => CarsLoaded());
+      when(refuelingsBloc.state).thenAnswer((_) => RefuelingsLoaded());
+
       await tester.pumpWidget(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider<EfficiencyStatsBloc>.value(
-              value: effBloc,
-            ),
-            BlocProvider<DrivingDistanceStatsBloc>.value(
-              value: driveBloc,
-            ),
-          ],
-          child: MaterialApp(
-            home: Scaffold(
-              body: StatisticsScreen(),
+        ChangeNotifierProvider<BasePrefService>.value(
+          value: pref,
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider<CarsBloc>.value(
+                value: carsBloc,
+              ),
+              BlocProvider<RefuelingsBloc>.value(
+                value: refuelingsBloc,
+              ),
+            ],
+            child: MaterialApp(
+              home: Scaffold(
+                body: StatisticsScreen(),
+              ),
             ),
           ),
         ),
       );
       await tester.pump();
-      expect(find.byType(FuelMileageHistory), findsOneWidget);
-      expect(find.byType(DrivingDistanceHistory), findsOneWidget);
+      expect(find.byType(FuelMileageChart), findsOneWidget);
+      expect(find.byType(DrivingDistanceChart), findsOneWidget);
     });
+
     testWidgets('loaded', (WidgetTester tester) async {
-      final refueling = Refueling(
-          id: '0',
-          mileage: 0,
-          amount: 10,
-          cost: 10.0,
-          efficiency: 1.0,
-          date: DateTime.fromMillisecondsSinceEpoch(0),
-          carName: 'test');
-      final effData = [
-        Series<FuelMileagePoint, DateTime>(
-          id: 'Fuel Mileage vs Time',
-          domainFn: (point, _) => point.date,
-          measureFn: (point, _) => point.efficiency,
-          data: [
-            FuelMileagePoint(refueling.date, refueling.efficiency),
-            FuelMileagePoint(DateTime.fromMillisecondsSinceEpoch(100), 2.0)
-          ],
-        ),
-        Series<FuelMileagePoint, DateTime>(
-          id: 'EMA',
-          domainFn: (point, _) => point.date,
-          measureFn: (point, _) => point.efficiency,
-          data: [
-            FuelMileagePoint(refueling.date, refueling.efficiency),
-            FuelMileagePoint(DateTime.fromMillisecondsSinceEpoch(100),
-                EfficiencyStatsBloc.emaFilter(1.0, 2.0))
-          ],
-        )
-      ];
-      final car = Car(name: 'abcd', distanceRateHistory: [
-        DistanceRatePoint(DateTime.fromMillisecondsSinceEpoch(0), 1.0),
-        DistanceRatePoint(DateTime.fromMillisecondsSinceEpoch(1000), 2.0),
-      ]);
-      final driveData = [
-        Series<DistanceRatePoint, DateTime>(
-          id: car.name,
-          domainFn: (point, _) => point.date,
-          measureFn: (point, _) => point.distanceRate,
-          data: car.distanceRateHistory,
-        )
-      ];
-      when(effBloc.state).thenAnswer((_) => EfficiencyStatsLoaded(effData));
-      when(driveBloc.state)
-          .thenAnswer((_) => DrivingDistanceStatsLoaded(driveData));
+      final rnd = Random(1234);
+      final carsBloc = MockCarsBloc();
+      final refuelingsBloc = MockRefuelingsBloc();
+
+      final refuelings = List<Refueling>.generate(
+          100,
+          (int index) => Refueling(
+              id: '$index',
+              mileage: index * 200.0,
+              amount: rnd.nextDouble() * 30 + 10,
+              cost: rnd.nextDouble() * 30 + 10,
+              efficiency: 1.0,
+              date: DateTime.fromMillisecondsSinceEpoch(
+                  1546300800000 + index * 864000000),
+              carName: 'test'));
+
+      final car = Car(
+          name: 'test',
+          distanceRateHistory: List<DistanceRatePoint>.generate(
+              100,
+              (int index) => DistanceRatePoint(
+                  DateTime.fromMillisecondsSinceEpoch(
+                      1546300800000 + index * 864000000),
+                  index / 20.0)));
+
+      when(carsBloc.state).thenAnswer((_) => CarsLoaded([car]));
+      when(refuelingsBloc.state)
+          .thenAnswer((_) => RefuelingsLoaded(refuelings));
+
       await tester.pumpWidget(
-        MultiBlocProvider(
-          providers: [
-            BlocProvider<EfficiencyStatsBloc>.value(
-              value: effBloc,
-            ),
-            BlocProvider<DrivingDistanceStatsBloc>.value(
-              value: driveBloc,
-            ),
-          ],
-          child: MaterialApp(
-            home: Scaffold(
-              body: StatisticsScreen(),
+        ChangeNotifierProvider<BasePrefService>.value(
+          value: pref,
+          child: MultiBlocProvider(
+            providers: [
+              BlocProvider<CarsBloc>.value(
+                value: carsBloc,
+              ),
+              BlocProvider<RefuelingsBloc>.value(
+                value: refuelingsBloc,
+              ),
+            ],
+            child: MaterialApp(
+              home: Scaffold(
+                body: StatisticsScreen(),
+              ),
             ),
           ),
         ),
       );
       await tester.pump();
-      expect(find.byType(FuelMileageHistory), findsOneWidget);
-      expect(find.byType(DrivingDistanceHistory), findsOneWidget);
+      expect(find.byType(FuelMileageChart), findsOneWidget);
+      expect(find.byType(DrivingDistanceChart), findsOneWidget);
     });
   });
 }
