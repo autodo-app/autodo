5cb21c38750ed57b73e2a6b5f604e8b6b3e8b9af
David PHAM-VAN <dev.nfet.net@gmail.com>
2020-03-21 16:33:04 -0400

    Use the about library to display the legal text

--- lib/screens/welcome/widgets/legal_notice.dart
+++ lib/screens/welcome/widgets/legal_notice.dart
@@ -1,30 +1,20 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/gestures.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'package:autodo/blocs/blocs.dart';
-import 'package:autodo/widgets/widgets.dart';
+import 'package:about/about.dart';
 import 'package:autodo/localization.dart';
 import 'package:autodo/theme.dart';
+import 'package:flutter/gestures.dart';
+import 'package:flutter/material.dart';
 import 'package:json_intl/json_intl.dart';
 
 class LegalNotice extends StatelessWidget {
-  void onTap(context) {
-    BlocProvider.of<LegalBloc>(context).add(LoadLegal());
-    showDialog<Widget>(
-        context: context,
-        builder: (ctx) =>
-            BlocBuilder<LegalBloc, LegalState>(builder: (context, state) {
-              if (state is LegalLoading) {
-                return LoadingIndicator();
-              } else if (state is LegalLoaded) {
-                return PrivacyPolicy(state.text);
-              } else {
-                Navigator.pop(context);
-                return Container();
-              }
-            }));
+  void _privacyPolicy(context) {
+    showMarkdownPage(
+      context: context,
+      title: Text(JsonIntl.of(context).get(IntlKeys.privacyPolicy)),
+      filename: 'legal/privacy-policy.md',
+    );
   }
+
+  void _termsAndConditions(context) {}
 
   @override
   Widget build(context) => Container(
@@ -40,7 +30,8 @@ class LegalNotice extends StatelessWidget {
           TextSpan(
             text: JsonIntl.of(context).get(IntlKeys.legal2),
             style: linkStyle(),
-            recognizer: TapGestureRecognizer()..onTap = () {},
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => _termsAndConditions(context),
           ),
           TextSpan(
             text: ' ${JsonIntl.of(context).get(IntlKeys.legal3)} ',
@@ -49,7 +40,8 @@ class LegalNotice extends StatelessWidget {
           TextSpan(
             text: JsonIntl.of(context).get(IntlKeys.legal4),
             style: linkStyle(),
-            recognizer: TapGestureRecognizer()..onTap = () => onTap(context),
+            recognizer: TapGestureRecognizer()
+              ..onTap = () => _privacyPolicy(context),
             semanticsLabel: 'Privacy Policy Button',
           ),
           TextSpan(
