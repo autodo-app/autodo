58cc4667c2f9d9d2457816da047aec4a7b8b59d1
Jonathan Bayless <baylessj@purdue.edu>
2020-03-31 18:57:32 -0400

    fix repeat interval add function

--- test/models/repeat_interval_test.dart
+++ test/models/repeat_interval_test.dart
@@ -6,7 +6,7 @@ void main() {
     test('addToDate', () {
       final epoch = DateTime.fromMillisecondsSinceEpoch(0);
       final interval = RepeatInterval(years: 1, months: 1, days: 1);
-      expect(interval.addToDate(epoch), DateTime(1971, 2, 1));
+      expect(interval.addToDate(epoch), DateTime(1971, 2, 2));
     });
   });
 }
