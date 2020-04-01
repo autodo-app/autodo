aa677cdc8a179f80f6b10e55d762499239f988ac
Jonathan Bayless <baylessj@purdue.edu>
2020-04-01 16:40:35 -0400

    add placeholder car image

--- lib/screens/home/widgets/car_card.dart
+++ lib/screens/home/widgets/car_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 
 import '../../../models/models.dart';
 
@@ -16,29 +17,40 @@ class CarCard extends StatelessWidget {
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.all(Radius.circular(15)),
         ),
-        child: Column(
-          children: <Widget>[
-            // Image.asset('car1.jpg'),
-            Padding(padding: EdgeInsets.all(5),),
-            Center(
-              child: Text(
-                '2015 Sonic',
-                style: Theme.of(context).primaryTextTheme.headline6),
-            ),
-            Padding(padding: EdgeInsets.all(5),),
-            Center(
-              child: Text(
-                'Plate: 23908u4',
-                style: Theme.of(context).primaryTextTheme.bodyText2)
-            ),
-            Center(
-              child: Text('Odom: safdlkj',
-                style: Theme.of(context).primaryTextTheme.bodyText2)
-            ),
-            Padding(padding: EdgeInsets.all(5),),
-            Icon(Icons.zoom_in),
-            Padding(padding: EdgeInsets.all(5),),
-          ],
+        child: SizedBox(
+          width: 140,
+          child: Column(
+            children: <Widget>[
+              CachedNetworkImage(
+                fit: BoxFit.fill,
+                height: 80,
+                placeholder: (context, url) => CircularProgressIndicator(),
+                errorWidget: (context, url, error) => Icon(Icons.error),
+                imageUrl: 'https://picsum.photos/250?image=9',
+              ),
+              Padding(padding: EdgeInsets.all(5),),
+              Center(
+                child: Text(
+                  '2015 Sonic',
+                  style: Theme.of(context).primaryTextTheme.headline6),
+              ),
+              Center(
+                child: Text(
+                  'Plate: 23908u4',
+                  style: Theme.of(context).primaryTextTheme.bodyText2)
+              ),
+              Center(
+                child: Text('Odom: safdlkj',
+                  style: Theme.of(context).primaryTextTheme.bodyText2)
+              ),
+              // Padding(padding: EdgeInsets.all(5),),
+              Align(
+                alignment: Alignment.centerRight,
+                child: Container(
+                  padding: EdgeInsets.all(8),
+                  child: Icon(Icons.zoom_in))),
+            ],
+          ),
         ),
       ),
     ),
