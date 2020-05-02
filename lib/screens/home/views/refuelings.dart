7e12afa8ef1cad4ef1eac86488f037213491b484
Jonathan Bayless <baylessj@purdue.edu>
2020-05-02 13:04:08 -0400

    use intl's date formatters and account for past month rollover

--- lib/screens/home/views/refuelings.dart
+++ lib/screens/home/views/refuelings.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:json_intl/json_intl.dart';
+import 'package:intl/intl.dart';
 
 import '../../../blocs/blocs.dart';
 import '../../../generated/localization.dart';
@@ -79,7 +80,8 @@ class _CalendarDay extends StatelessWidget {
           child: Column(
             children: [
               Text(
-                JsonIntl.of(context).get(name),
+                name,
+                // JsonIntl.of(context).get(name),
                 style: _textStyle(context),
               ),
               Text(
@@ -92,37 +94,32 @@ class _CalendarDay extends StatelessWidget {
           )));
 }
 
-const weekdays = [
-  IntlKeys.sunShort,
-  IntlKeys.monShort,
-  IntlKeys.tueShort,
-  IntlKeys.wedShort,
-  IntlKeys.thuShort,
-  IntlKeys.friShort,
-  IntlKeys.satShort,
-];
-
 class _CalendarView extends StatelessWidget {
   const _CalendarView({Key key, @required this.refuelings}) : super(key: key);
 
   final List<Refueling> refuelings;
 
-  bool _refuelingToday(DateTime date) => refuelings
+  bool _refuelingToday(DateTime date) {
+    return refuelings
       .any((r) => roundToDay(r.date).isAtSameMomentAs(roundToDay(date)));
+  }
 
   DateTime _getInterval(int dateDay, int dayRangeVal) =>
       RepeatInterval(days: dayRangeVal - dateDay).addToDate(DateTime.now());
 
   /// Returns the day value adjusted so that values from the upcoming month
-  /// are correct.
+  /// and past month are correct.
   ///
   /// Using the next month with a day value of 0 returns the last day of the
   /// current month.
   int _checkRollover(int day) {
-    final lastDay =
+    final thisLastDay =
         DateTime(DateTime.now().year, DateTime.now().month + 1, 0).day;
-    if (day > lastDay) {
-      return day - lastDay;
+    final pastLastDay = DateTime(DateTime.now().year, DateTime.now().month, 0).day;
+    if (day > thisLastDay) {
+      return day - thisLastDay;
+    } else if (day < 1) {
+      return day + pastLastDay;
     }
     return day;
   }
@@ -131,8 +128,12 @@ class _CalendarView extends StatelessWidget {
   Widget build(BuildContext context) {
     final dayOfWeek = DateTime.now().weekday;
     final dateDay = DateTime.now().day;
-    final dayRange = List.generate(
-        7, (index) => _checkRollover(index + dateDay - dayOfWeek));
+    // Raw range is used to calculate the DateTime for each day when searching
+    // for matching refuelings
+    final dayRangeRaw = List.generate(7, (index) => index + dateDay - dayOfWeek);
+    // The display day range accounts for past and future month rollovers so the
+    // dates are correct per the calendar
+    final displayDayRange = dayRangeRaw.map(_checkRollover).toList();
 
     return ClipRect(
         child: Container(
@@ -143,10 +144,14 @@ class _CalendarView extends StatelessWidget {
               children: List.generate(
                   7, // days in a week
                   (index) => _CalendarDay(
-                      name: weekdays[index],
-                      number: dayRange[index],
+                      name: DateFormat
+                          .E()
+                          .format(DateTime.now()
+                          .subtract(Duration(days: dayOfWeek))
+                          .add(Duration(days: index))),
+                      number: displayDayRange[index],
                       refuelingToday: _refuelingToday(
-                          _getInterval(dateDay, dayRange[index])))),
+                          _getInterval(dateDay, dayRangeRaw[index])))),
             )));
   }
 }
