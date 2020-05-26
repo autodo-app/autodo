6cc83c3f7d465d1653bd9f0ee25ed7c356ae6677
Jonathan Bayless <baylessj@purdue.edu>
2020-05-26 15:22:33 -0400

    enable api versioning through url

--- api/settings.py
+++ api/settings.py
@@ -133,4 +133,5 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_VERSIONING_CLASS': 'rest_framework.versioning.URLPathVersioning',
 }
