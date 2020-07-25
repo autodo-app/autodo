7b9120de0e257cbe5008d1197bb01e01767548f3
Jonathan Bayless <baylessj@purdue.edu>
2020-07-24 20:47:09 -0400

    accessing api locally

--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import MyComponent from './api';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <MyComponent />
   </React.StrictMode>,
   document.getElementById('root')
 );
