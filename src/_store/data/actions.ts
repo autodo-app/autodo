e575a2312201ee6bf4ea733316bb8098eb496fa1
Jonathan Bayless <baylessj@purdue.edu>
2020-08-08 15:40:59 -0400

    add redux actions

--- src/_store/data/actions.ts
+++ src/_store/data/actions.ts
@@ -1,42 +1,20 @@
-import { Todo } from '../../_models';
-import {
-  FETCH_DATA,
-  CREATE_TODO,
-  UPDATE_TODO,
-  DELETE_TODO,
-  COMPLETE_TODO,
-  UNCOMPLETE_TODO,
-  FetchDataAction,
-  CompleteTodoAction,
-  UnCompleteTodoAction,
-  DeleteTodoAction,
-  UpdateTodoAction,
-  CreateTodoAction,
-} from './types';
-import {
-  fetchTodos,
-  fetchRefuelings,
-  fetchCars,
-  apiPostTodo,
-  apiPatchTodo,
-  apiDeleteTodo,
-  apiPostOdomSnapshot,
-  apiDeleteOdomSnapshot,
-} from '../../_services';
+import { Todo, Refueling, Car } from '../../_models';
+import * as types from './types';
+import * as api from '../../_services';
 import { createAsyncThunk } from '@reduxjs/toolkit';
 
 import { RootState } from '../../app/store';
 
-export const fetchData = createAsyncThunk<FetchDataAction, void>(
-  FETCH_DATA,
+export const fetchData = createAsyncThunk<types.FetchDataAction, void>(
+  types.FETCH_DATA,
   async () => {
     const [todos, refuelings, cars] = await Promise.all([
-      fetchTodos(),
-      fetchRefuelings(),
-      fetchCars(),
+      api.fetchTodos(),
+      api.fetchRefuelings(),
+      api.fetchCars(),
     ]);
     return {
-      type: FETCH_DATA,
+      type: types.FETCH_DATA,
       payload: {
         todos: todos,
         refuelings: refuelings,
@@ -45,55 +23,55 @@ export const fetchData = createAsyncThunk<FetchDataAction, void>(
         status: '',
         error: '',
       },
-    } as FetchDataAction;
+    } as types.FetchDataAction;
   },
 );
 
-export const createTodo = createAsyncThunk<CreateTodoAction, Todo>(
-  CREATE_TODO,
+export const createTodo = createAsyncThunk<types.CreateTodoAction, Todo>(
+  types.CREATE_TODO,
   async (initialTodo) => {
-    const response = await apiPostTodo(initialTodo);
+    const response = await api.postTodo(initialTodo);
     return {
-      type: CREATE_TODO,
+      type: types.CREATE_TODO,
       payload: response,
-    } as CreateTodoAction;
+    } as types.CreateTodoAction;
   },
 );
 
-export const updateTodo = createAsyncThunk<UpdateTodoAction, Todo>(
-  UPDATE_TODO,
+export const updateTodo = createAsyncThunk<types.UpdateTodoAction, Todo>(
+  types.UPDATE_TODO,
   async (initialTodo) => {
-    const response = await apiPatchTodo(initialTodo);
+    const response = await api.patchTodo(initialTodo);
     return {
-      type: UPDATE_TODO,
+      type: types.UPDATE_TODO,
       payload: response,
-    } as UpdateTodoAction;
+    } as types.UpdateTodoAction;
   },
 );
 
-export const deleteTodo = createAsyncThunk<DeleteTodoAction, Todo>(
-  DELETE_TODO,
+export const deleteTodo = createAsyncThunk<types.DeleteTodoAction, Todo>(
+  types.DELETE_TODO,
   async (todo) => {
-    const response = await apiDeleteTodo(todo);
+    const response = await api.deleteTodo(todo);
     return {
-      type: DELETE_TODO,
+      type: types.DELETE_TODO,
       payload: response,
-    } as DeleteTodoAction;
+    } as types.DeleteTodoAction;
   },
 );
 
 export const completeTodo = createAsyncThunk<
-  CompleteTodoAction,
+  types.CompleteTodoAction,
   Todo,
   { state: RootState }
->(COMPLETE_TODO, async (initialTodo, thunkApi) => {
+>(types.COMPLETE_TODO, async (initialTodo, thunkApi) => {
   const car = thunkApi
     .getState()
     .data.cars.find((c) => Number(c.id) === Number(initialTodo.car));
   if (!car) {
     return {
-      type: COMPLETE_TODO,
-    } as CompleteTodoAction;
+      type: types.COMPLETE_TODO,
+    } as types.CompleteTodoAction;
   }
   const curMileage = car.odom;
   const initialSnapshot = {
@@ -101,32 +79,98 @@ export const completeTodo = createAsyncThunk<
     date: new Date(),
     mileage: curMileage,
   };
-  const odomSnapshot = await apiPostOdomSnapshot(initialSnapshot);
+  const odomSnapshot = await api.postOdomSnapshot(initialSnapshot);
   const updatedTodo = {
     ...initialTodo,
     completionOdomSnapshot: odomSnapshot,
   };
-  const response = await apiPatchTodo(updatedTodo);
+  const response = await api.patchTodo(updatedTodo);
   return {
-    type: COMPLETE_TODO,
+    type: types.COMPLETE_TODO,
     payload: response,
-  } as CompleteTodoAction;
+  } as types.CompleteTodoAction;
 });
 
-export const undoCompleteTodo = createAsyncThunk<UnCompleteTodoAction, Todo>(
-  UNCOMPLETE_TODO,
-  async (initialTodo) => {
-    if (!initialTodo.completionOdomSnapshot?.id) {
-      // there is not a valid odomSnapshot on the todo to undo
-      return {
-        type: UNCOMPLETE_TODO,
-      } as UnCompleteTodoAction;
-    }
-    await apiDeleteOdomSnapshot(initialTodo.completionOdomSnapshot.id);
-    const response = await fetchTodos();
+export const undoCompleteTodo = createAsyncThunk<
+  types.UnCompleteTodoAction,
+  Todo
+>(types.UNCOMPLETE_TODO, async (initialTodo) => {
+  if (!initialTodo.completionOdomSnapshot?.id) {
+    // there is not a valid odomSnapshot on the todo to undo
     return {
-      type: UNCOMPLETE_TODO,
+      type: types.UNCOMPLETE_TODO,
+    } as types.UnCompleteTodoAction;
+  }
+  await api.deleteOdomSnapshot(initialTodo.completionOdomSnapshot.id);
+  const response = await api.fetchTodos();
+  return {
+    type: types.UNCOMPLETE_TODO,
+    payload: response,
+  } as types.UnCompleteTodoAction;
+});
+
+export const createRefueling = createAsyncThunk<
+  types.CreateRefuelingAction,
+  Refueling
+>(types.CREATE_REFUELING, async (initialRefueling) => {
+  const response = await api.postRefueling(initialRefueling);
+  return {
+    type: types.CREATE_REFUELING,
+    payload: response,
+  } as types.CreateRefuelingAction;
+});
+
+export const updateRefueling = createAsyncThunk<
+  types.UpdateRefuelingAction,
+  Refueling
+>(types.UPDATE_REFUELING, async (initialRefueling) => {
+  const response = await api.patchRefueling(initialRefueling);
+  return {
+    type: types.UPDATE_REFUELING,
+    payload: response,
+  } as types.UpdateRefuelingAction;
+});
+
+export const deleteRefueling = createAsyncThunk<
+  types.DeleteRefuelingAction,
+  Refueling
+>(types.DELETE_REFUELING, async (initialRefueling) => {
+  const response = await api.deleteRefueling(initialRefueling);
+  return {
+    type: types.DELETE_REFUELING,
+    payload: response,
+  } as types.DeleteRefuelingAction;
+});
+
+export const createCar = createAsyncThunk<types.CreateCarAction, Car>(
+  types.CREATE_CAR,
+  async (initialCar) => {
+    const response = await api.postCar(initialCar);
+    return {
+      type: types.CREATE_CAR,
       payload: response,
-    } as UnCompleteTodoAction;
+    } as types.CreateCarAction;
+  },
+);
+
+export const updateCar = createAsyncThunk<types.UpdateCarAction, Car>(
+  types.UPDATE_CAR,
+  async (initialCar) => {
+    const response = await api.patchCar(initialCar);
+    return {
+      type: types.UPDATE_CAR,
+      payload: response,
+    } as types.UpdateCarAction;
+  },
+);
+
+export const deleteCar = createAsyncThunk<types.DeleteCarAction, Car>(
+  types.DELETE_CAR,
+  async (initialCar) => {
+    const response = await api.deleteCar(initialCar);
+    return {
+      type: types.DELETE_CAR,
+      payload: response,
+    } as types.DeleteCarAction;
   },
 );
--- src/_store/data/types.ts
+++ src/_store/data/types.ts
@@ -1,5 +1,5 @@
 import { DataState } from './state';
-import { Todo } from '../../_models';
+import { Todo, Refueling, Car } from '../../_models';
 
 export const FETCH_DATA = 'FETCH_DATA';
 export const CREATE_TODO = 'CREATE_TODO';
@@ -7,6 +7,12 @@ export const UPDATE_TODO = 'UPDATE_TODO';
 export const DELETE_TODO = 'DELETE_TODO';
 export const COMPLETE_TODO = 'COMPLETE_TODO';
 export const UNCOMPLETE_TODO = 'UNCOMPLETE_TODO';
+export const CREATE_REFUELING = 'CREATE_REFUELING';
+export const UPDATE_REFUELING = 'UPDATE_REFUELING';
+export const DELETE_REFUELING = 'DELETE_REFUELING';
+export const CREATE_CAR = 'CREATE_CAR';
+export const UPDATE_CAR = 'UPDATE_CAR';
+export const DELETE_CAR = 'DELETE_CAR';
 
 export interface FetchDataAction {
   type: typeof FETCH_DATA;
@@ -38,10 +44,46 @@ export interface UnCompleteTodoAction {
   payload: Todo[];
 }
 
+export interface CreateRefuelingAction {
+  type: typeof CREATE_REFUELING;
+  payload: Refueling;
+}
+
+export interface UpdateRefuelingAction {
+  type: typeof UPDATE_REFUELING;
+  payload: Refueling;
+}
+
+export interface DeleteRefuelingAction {
+  type: typeof DELETE_REFUELING;
+  payload: Refueling;
+}
+
+export interface CreateCarAction {
+  type: typeof CREATE_CAR;
+  payload: Car;
+}
+
+export interface UpdateCarAction {
+  type: typeof UPDATE_CAR;
+  payload: Car;
+}
+
+export interface DeleteCarAction {
+  type: typeof DELETE_CAR;
+  payload: Car;
+}
+
 export type DataActionTypes =
   | FetchDataAction
   | CreateTodoAction
   | UpdateTodoAction
   | DeleteTodoAction
   | CompleteTodoAction
-  | UnCompleteTodoAction;
+  | UnCompleteTodoAction
+  | CreateRefuelingAction
+  | UpdateRefuelingAction
+  | DeleteRefuelingAction
+  | CreateCarAction
+  | UpdateCarAction
+  | DeleteCarAction;
