3de9e6cee7991f705545740afb2445dfe404db44
Jonathan Bayless <baylessj@purdue.edu>
2020-08-15 14:07:27 -0400

    move all of the mobile app stuff to its own folder

---
+++ mobile/legal/README.md
@@ -0,0 +1,2 @@
+# legal
+The auToDo app's privacy policy, terms and conditions, and other legal information shared between the web and app.
---
+++ mobile/lib/generated/keys.dart
@@ -0,0 +1,12 @@
+// This file is generated automatically, do not modify
+
+class Keys {
+  static const String sentryDsn =
+      'https://20c77d8ea8634cb68f64d31a70a43add@sentry.io/1865109';
+
+  static const String firebaseTestKey =
+      'AIzaSyAAYhwsJVyiYywUFORBgaUuyXqXFiFpbZo';
+
+  static const String firebaseProdKey =
+      'AIzaSyAOPBHmp7KUXjjnWKFRAWP3z48B90iBruM';
+}
