437e7f66f67a19138045c3e21cca179c424aa297
Jonathan Bayless <baylessj@purdue.edu>
2020-09-13 16:12:10 -0400

    set favicon properly

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,6 +20,7 @@ module.exports = {
       resolve: `gatsby-plugin-manifest`,
       options: {
         name: `gatsby-starter-default`,
+        icon: `${__dirname}/src/assets/icon-green.svg`,
         short_name: `starter`,
         start_url: `/`,
         background_color: `#663399`,
