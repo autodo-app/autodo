fa3174c9b0810c8cd5eb5c107f6c4888a6288ded
baylessj <baylessj@purdue.edu>
2020-09-19 19:57:44 +0000

    remove the built public files

--- frontend/public/component---cache-caches-gatsby-plugin-offline-app-shell-js-261c76997bfd9b7644f9.js
+++
@@ -1,2 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[4],{zXQ9:function(e,t,n){"use strict";var u=n("Fc5S");t.__esModule=!0,t.default=void 0;var o=u(n("AT6c")),r=u(n("Iz2L")),a=function(e){function t(){return e.apply(this,arguments)||this}return(0,o.default)(t,e),t.prototype.render=function(){return r.default.createElement(r.default.Fragment,null)},t}(r.default.Component);t.default=a}}]);
-//# sourceMappingURL=component---cache-caches-gatsby-plugin-offline-app-shell-js-261c76997bfd9b7644f9.js.map
