7aca56b04235886b76000c2a2f33140b05672d2c
Jonathan Bayless <baylessj@purdue.edu>
2020-09-20 17:05:55 -0400

    apparently the black action only checks the files

--- backend/autodo/migrations/0004_auto_20200913_1911.py
+++ backend/autodo/migrations/0004_auto_20200913_1911.py
@@ -7,42 +7,47 @@ import django.db.models.deletion
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('autodo', '0003_auto_20200529_0110'),
+        ("autodo", "0003_auto_20200529_0110"),
     ]
 
     operations = [
         migrations.RemoveField(
-            model_name='car',
-            name='imageName',
+            model_name="car",
+            name="imageName",
         ),
         migrations.AddField(
-            model_name='car',
-            name='image',
-            field=models.ImageField(null=True, upload_to='cars'),
+            model_name="car",
+            name="image",
+            field=models.ImageField(null=True, upload_to="cars"),
         ),
         migrations.AlterField(
-            model_name='car',
-            name='make',
+            model_name="car",
+            name="make",
             field=models.CharField(blank=True, max_length=32, null=True),
         ),
         migrations.AlterField(
-            model_name='car',
-            name='model',
+            model_name="car",
+            name="model",
             field=models.CharField(blank=True, max_length=32, null=True),
         ),
         migrations.AlterField(
-            model_name='car',
-            name='name',
+            model_name="car",
+            name="name",
             field=models.CharField(max_length=32),
         ),
         migrations.AlterField(
-            model_name='todo',
-            name='completionOdomSnapshot',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.SET_NULL, to='autodo.OdomSnapshot'),
+            model_name="todo",
+            name="completionOdomSnapshot",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.SET_NULL,
+                to="autodo.OdomSnapshot",
+            ),
         ),
         migrations.AlterField(
-            model_name='todo',
-            name='name',
+            model_name="todo",
+            name="name",
             field=models.CharField(max_length=32),
         ),
     ]
