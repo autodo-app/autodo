0fd6654e2898714a740a9db84aea7b1e325b1f7b
Jonathan Bayless <baylessjonathan@gmail.com>
2021-02-23 19:42:36 -0500

    pull old models

--- autodo/views.py
+++ autodo/views.py
@@ -7,10 +7,7 @@ from .models import Greeting
 # Create your views here.
 def index(request):
     # return HttpResponse('Hello from Python!')
-    r = requests.get('http://httpbin.org/status/418')
-    print(r.text)
-    return HttpResponse('<pre>' + r.text + '</pre>')
-    # return render(request, "index.html")
+    return render(request, "index.html")
 
 
 def db(request):
--- main/settings.py
+++ main/settings.py
@@ -95,6 +95,7 @@ AUTH_PASSWORD_VALIDATORS = [
     {"NAME": "django.contrib.auth.password_validation.NumericPasswordValidator"},
 ]
 
+AUTH_USER_MODEL = "autodo.User"
 
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
