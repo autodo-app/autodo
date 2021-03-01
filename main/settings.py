d2432824e7259b091e0d92c5272abba524f0b2a8
Jonathan Bayless <baylessjonathan@gmail.com>
2021-02-28 20:21:30 -0500

    resolve path issues

--- main/settings.py
+++ main/settings.py
@@ -15,7 +15,7 @@ import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 BACKEND_DIR = BASE_DIR
 FRONTEND_DIR = os.path.abspath(
@@ -60,7 +60,7 @@ ROOT_URLCONF = "main.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [os.path.join(BASE_DIR, "templates")],
+        "DIRS": [os.path.join(BASE_DIR, "..", "templates")],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -82,7 +82,7 @@ WSGI_APPLICATION = "main.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE" : "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3")
+        "NAME": os.path.join(BASE_DIR, '..', "db.sqlite3")
     }
 }
 
@@ -117,8 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
-# STATICFILES_DIRS = [os.path.join(FRONTEND_DIR, 'build', 'static')]
-STATIC_ROOT = os.path.join(BACKEND_DIR, 'staticfiles')
+STATICFILES_DIRS = [os.path.join(BASE_DIR, '..', 'staticfiles')]
+STATIC_ROOT = os.path.join(BACKEND_DIR, '..', 'staticfiles')
 STATIC_URL = '/static/'  # already declared in the default settings
 
 django_heroku.settings(locals())
