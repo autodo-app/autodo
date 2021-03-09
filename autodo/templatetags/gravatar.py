f5e51106bbb061c28dcd55692c7561780f329822
Jonathan Bayless <baylessjonathan@gmail.com>
2021-03-09 16:09:48 -0500

    use identicon for pfp

---
+++ autodo/templatetags/gravatar.py
@@ -0,0 +1,22 @@
+import hashlib
+from urllib.parse import urlencode
+
+from django import template
+from django.conf import settings
+
+register = template.Library()
+
+
+@register.filter
+def gravatar(user):
+    try:
+        email = user.email.lower().encode('utf-8')
+    except AttributeError:
+        return ""
+    default = 'retro'
+    size = 40
+    url = 'https://www.gravatar.com/avatar/{md5}?{params}'.format(
+        md5=hashlib.md5(email).hexdigest(),
+        params=urlencode({'d': default, 's': str(size)})
+    )
+    return url
