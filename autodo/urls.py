6f1368e592362dbbaeb877ae43d3048a61a6719f
Jonathan Bayless <baylessjonathan@gmail.com>
2021-03-11 17:10:27 -0500

    tailwindui sign-in form

--- autodo/urls.py
+++ autodo/urls.py
@@ -1,13 +1,15 @@
 from django.urls import path, include
 
-from autodo.views import ListView, DetailView, CarCreate, CarUpdate, CarDelete, catchall 
+from autodo.views import ListView, DetailView, CarCreate, CarUpdate, CarDelete, catchall
 
 urlpatterns = [
-  path("", catchall),
-  path("accounts/", include("django.contrib.auth.urls")),
-  path("cars/", ListView.as_view(), name="cars"),
-  path("cars/create/", CarCreate.as_view(), name="cars/create"),
-  path("cars/<int:pk>/", DetailView.as_view(), name="cars/detail"),
-  path("cars/<int:pk>/update/", CarUpdate.as_view(), name="cars/update"),
-  path("cars/<int:pk>/delete/", CarDelete.as_view(), name="cars/delete"),
+    path("", catchall),
+    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/profile/", catchall, name="profile"),
+    path("settings", catchall, name="settings"),
+    path("cars/", ListView.as_view(), name="cars"),
+    path("cars/create/", CarCreate.as_view(), name="cars/create"),
+    path("cars/<int:pk>/", DetailView.as_view(), name="cars/detail"),
+    path("cars/<int:pk>/update/", CarUpdate.as_view(), name="cars/update"),
+    path("cars/<int:pk>/delete/", CarDelete.as_view(), name="cars/delete"),
 ]
