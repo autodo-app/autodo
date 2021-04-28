7a9fbebbe3a4261d61b2bb127ad61c70f1164a42
Jonathan Bayless <baylessjonathan@gmail.com>
2021-04-28 18:55:17 -0400

    styling and upcoming as the default

--- autodo/filters.py
+++ autodo/filters.py
@@ -3,17 +3,34 @@ from django import forms
 
 from autodo.models import Todo
 
+default_form_classes = [
+    "bg-gray-50",
+    "my-2",
+    "py-1",
+    "pl-2",
+    "pr-6",
+    "border",
+    "border-gray-400",
+    "rounded",
+    "focus:outline-none",
+    "focus:border-blue-500",
+    "placeholder-gray-500",
+]
+default_form_class = " ".join(default_form_classes)
+
 
 class TodoFilter(django_filters.FilterSet):
     complete = django_filters.BooleanFilter(
         field_name="complete",
+        label="Filter",
+        initial=False,
         widget=forms.Select(
             choices=[
-                ("", "Unknown"),
-                (True, "Yes"),
-                (False, "No"),
+                ("", "All"),
+                (True, "Completed"),
+                (False, "Upcoming"),
             ],
-            attrs={"onchange": "this.form.submit()"},
+            attrs={"onchange": "this.form.submit()", "class": default_form_class},
         ),
     )
     # due_date = django_filters.DateTimeFilter(field_name="dueDate", lookup_expr="lt")
