897a0c21445d364f88461703db40aec637c9aec1
Jonathan Bayless <baylessjonathan@gmail.com>
2021-05-02 15:48:10 -0400

    looks like staticfiles are the issue

--- main/urls.py
+++ main/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
-
+from django.views.static import serve
 from django.contrib import admin
 
 admin.autodiscover()
 
 urlpatterns = [
     path("", include("autodo.urls")),
+    path("static/", serve, {"document_root": settings.STATIC_ROOT}),
     path("admin/", admin.site.urls),
 ]
